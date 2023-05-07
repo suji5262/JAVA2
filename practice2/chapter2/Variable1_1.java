1f82ace35bf22f167d1359d5e9e4886b43b9ac54
suji5262 <suji5262@gmail.com>
2023-05-08 01:02:19 +0900

    :sparkles: Variable1_1  file add

---
+++ practice2/chapter2/Variable1_1.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class Variable1_1 {
+    public static void main(String[] args) {
+
+        int age;
+        age = 32;
+        System.out.println(age);
+    } // 복습문제
+}
