1f7cc2d9474e8e439c489a68424bf510c8960fef
suji5262 <suji5262@gmail.com>
2023-05-09 17:57:15 +0900

    :sparkles: Variable2  file add

---
+++ practice2/chapter2/Variable2.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class Variable2 {
+    public static void main(String[] args) {
+
+        // 변수 초기화
+        int level = 10;
+        System.out.println(level);
+    }
+}
