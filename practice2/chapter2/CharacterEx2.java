b910b161354eadea6f36289d4ba8aadf59c8ed2c
suji5262 <suji5262@gmail.com>
2023-05-10 23:52:01 +0900

    :sparkles: CharacterEx2 file add

---
+++ practice2/chapter2/CharacterEx2.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+public class CharacterEx2 {
+
+    public static void main(String[] args) {
+
+        // 문자를 사용할 때는 '' -> 작은따옴표를 사용
+        // 문자열을 사용할 때는 "" -> 큰따옴표 사용
+
+        char ch1 = '한';
+        char ch2 = '\uD55C'; // 유니코드 값
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+    }
+}
