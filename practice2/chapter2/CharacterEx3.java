3476c43d1f2704606f177932f8f9ad933209bc58
suji5262 <suji5262@gmail.com>
2023-05-13 21:17:09 +0900

    :sparkles: CharacterEx3 file add

---
+++ practice2/chapter2/CharacterEx3.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+public class CharacterEx3 {
+
+    public static void main(String[] args) {
+
+        int a = 65;
+        int b = -66;
+
+        char a2 = 65;
+        // char b2 = -66; //char 형은 음수값을 사용할 수 없음
+
+        System.out.println((char)a);
+        System.out.println((char)b);
+        System.out.println(a2);
+
+    }
+}
