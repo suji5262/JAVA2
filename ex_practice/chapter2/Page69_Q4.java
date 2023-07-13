d24ec7a10579c6a3f96a6db6cd23c9bbde7a9e07
suji5262 <suji5262@gmail.com>
2023-07-14 03:11:22 +0900

    :sparkles: Page69_Q4 file add

---
+++ ex_practice/chapter2/Page69_Q4.java
@@ -0,0 +1,14 @@
+package chapter2;
+
+public class Page69_Q4 {
+    public static void main(String[] args) {
+
+        int i = 10;
+        double d = 2.0;
+
+        System.out.println((int)(i + d));
+        System.out.println((int)(i - d));
+        System.out.println((int)(i * d));
+        System.out.println((int)(i / d));
+    }
+}
