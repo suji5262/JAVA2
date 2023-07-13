32178d9c055402e8cc0e3a72d77e164d475b069f
suji5262 <suji5262@gmail.com>
2023-07-14 03:10:59 +0900

    :sparkles: CharacterEx4 file add

---
+++ practice2/chapter2/CharacterEx4.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class CharacterEx4 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println((int)ch1);
+
+        int ch2 = 67;
+        System.out.println((char)ch2);
+    }
+}
